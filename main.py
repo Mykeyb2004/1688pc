25c1769d8226ac44aa99417a1145421649aaf530
Mykeyb2004 <keybchaos@gmail.com>
2019-07-19 19:25:11 +0800

    update codes

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 
 from crawler import Crawler
+from config import DEBUG
 
 if __name__ == '__main__':
     crawler = Crawler()
     crawler.crawl_pages()
     print("Job is done.")
+    if DEBUG:
+        print("采集程序处于调试模式，仅仅采集部分信息。")
