1f5d36256fa5388a58a39c70b574531377f69767
Mykeyb2004 <keybchaos@gmail.com>
2019-07-21 12:50:34 +0800

    根据操作系统指定不同的chromedriver

--- ads_url.py
+++ ads_url.py
@@ -2,6 +2,8 @@
 # -*- coding: UTF-8 -*-
 
 
+import os
+import platform
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -11,7 +13,13 @@ class AdsUrl:
     def __init__(self):
         chrome_options = Options()
         chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        self.driver = webdriver.Chrome(chrome_options=chrome_options)
+
+        # 根据操作系统指定不同的chromedriver
+        if platform.system() == 'Windows':
+            chrome_driver = os.path.abspath('.') + '\\chromedriver.exe'
+        else:
+            chrome_driver = "/usr/lib/chromium-browser/chromedriver"
+        self.driver = webdriver.Chrome(executable_path=chrome_driver, chrome_options=chrome_options)
 
     def get_url(self, ads_url):
         self.driver.get(ads_url)
