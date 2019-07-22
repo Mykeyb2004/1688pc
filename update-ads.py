731fdf61b30a919ef0f50f6743b23a67335e2a34
Mykeyb2004 <keybchaos@gmail.com>
2019-07-22 11:16:24 +0800

    加入启动提示信息

--- update-ads.py
+++ update-ads.py
@@ -5,6 +5,7 @@
 from convert_db import Converter
 
 if __name__ == '__main__':
+    print("Start to detect ads url...")
     converter = Converter()
     converter.convert_ads_url()
     print("Job is done.")
