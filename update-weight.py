5b7a9604adf6707c95e5117d0972d2b939b8bd3a
Mykeyb2004 <keybchaos@gmail.com>
2019-07-23 21:37:50 +0800

    增加了json文件结构说明信息

--- update-weight.py
+++ update-weight.py
@@ -4,8 +4,20 @@
 
 from analyzer import Analyzer
 
+# from weight_it import CalculateWeight
+
 if __name__ == '__main__':
-    print("Start to load data...")
+    print("Json文件的权重计算规则说明：")
+    print("=" * 30)
+    print("""
+    【sql】：\t查询语句
+    【top_rank_fields】：\t以最大值为最优值的权重计算字段
+    【percent_rank_fields】：\t以分位数为最优值的权重计算字段
+    【best_percent_rank】：\t指定的最优分位数
+    【best_value_rank】：\t指定固定数值为最优值。若为非零值，则忽略【percent_rank_fields】分位数。
+    【weight】：\t各字段的加权权重，总和最好为100。
+    """)
+    print("=" * 30)
     analyzer = Analyzer()
     analyzer.caculate_weight()
     print("Job is done.")
